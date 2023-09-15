8fb892e768f8ef1ddafdd459fa4ed11789ea208a
heyyouhere <heyyouhere@hyh.com>
2023-09-15 19:46:44 +0300

    Serial gives error, so i commented it out

--- flask_server.py
+++ flask_server.py
@@ -4,9 +4,9 @@ import serial
 app = Flask(__name__)
 
 #____________COM PORT SETTINGS_________________________#
-serialPort = None
-print('Search...')
-ports = serial.tools.list_ports.comports(include_links=False)
+#serialPort = None
+#print('Search...')
+#ports = serial.tools.list_ports.comports(include_links=False)
 #for port in ports :
 #    print('Find port '+ port.device)    
 #    serialPort = port.device
