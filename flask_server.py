e795b1a0696ada92a35900d88962f0e136298fec
heyyouhere <heyyouhere@hyh.com>
2023-09-15 19:44:00 +0300

    moved some files, disable serial

--- flask_server.py
+++ flask_server.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 serialPort = None
 print('Search...')
 ports = serial.tools.list_ports.comports(include_links=False)
-for port in ports :
-    print('Find port '+ port.device)    
-    serialPort = port.device
+#for port in ports :
+#    print('Find port '+ port.device)    
+#    serialPort = port.device
 
 
 @app.route('/')
